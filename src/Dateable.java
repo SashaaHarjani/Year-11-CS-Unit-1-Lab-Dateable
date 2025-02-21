2fa168814af199e51006fa6c811e932dbfc2e9b6
sashavinodharjani <sashavh2027@student.cis.edu.hk>
2025-02-21 11:51:03 +0800

    Lab 1 completed! :)

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
 public class Dateable {
-
   public static void main(String[] args) {
       // Write your code below, inside the main method.
-    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter you age:");
+    int age1 = scanner.nextInt();
+    int age2 = (7+(age1/2));
+    System.out.println(age1+"-years olds should date somebody who is at least "+age2+" years old.");
+
   }
-  
+
 }
